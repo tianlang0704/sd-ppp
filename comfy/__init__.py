9cc7edcace50d3eef8142c354c5c3e8b02bb29a4
CMonk <tianlang0704@gmail.com>
2024-05-08 04:22:36 +0800

    Add auto install plugin

--- comfy/__init__.py
+++ comfy/__init__.py
@@ -1,5 +1,7 @@
 
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 from . import apis
+from .utils import auto_install_ps_plugin
+auto_install_ps_plugin()
 WEB_DIRECTORY = 'comfy/static'
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
